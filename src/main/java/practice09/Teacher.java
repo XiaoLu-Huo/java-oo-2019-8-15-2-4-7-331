39c5936e6bad21c358f439c4e2202f99455b2adb
huoxiaolu <1821100432@qq.com>
2021-09-11 14:33:02 +0800

    [huoxiaolu] feat : 完成practice09

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,40 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private static final String TEACHER = " I am a Teacher.";
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce();
+        if (klass == null) {
+            return introduce + TEACHER + " I teach No Class.";
+        }
+        return introduce + TEACHER + " I teach " + klass.getDisplayName() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        String introduce = super.introduce();
+        if (student.getKlass().getNumber().equals(klass.getNumber())) {
+            return introduce + TEACHER + " I teach " + student.getName() + ".";
+        }
+        return introduce + TEACHER + " I don't teach " + student.getName() + ".";
+    }
 }
