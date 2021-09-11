71d76edb83cf9d313eddc0ee393863c036f25ebf
huoxiaolu <1821100432@qq.com>
2021-09-11 15:30:33 +0800

    [huoxiaolu] feat : 完成practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,73 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+
+    private final List<Observer> observers = new ArrayList<>();
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    protected void notifyObservers(Student student, NoticeType noticeType) {
+        for (Observer observer : observers) {
+            observer.update(this, student, noticeType);
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number.equals(klass.number);
+    }
+
+
+    public void assignLeader(Student leader) {
+        if (this.equals(leader.getKlass())) {
+            this.leader = leader;
+            notifyObservers(leader, NoticeType.BECOME_LEADER);
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        notifyObservers(student, NoticeType.JOIN_CLASS);
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public boolean isIn(Student student) {
+        return this.equals(student.getKlass());
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,74 @@
 package practice11;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person implements Observer{
+    private static final String TEACHER = " I am a Teacher.";
+    private LinkedList<Klass> klass;
+
+    @Override
+    public void update(Klass klass, Student student, NoticeType noticeType) {
+        if (noticeType == NoticeType.JOIN_CLASS) {
+            System.out.println("I am " + getName() + ". I know " + student.getName() + " has joined Class " + klass.getNumber() + ".");
+        }
+        if (noticeType == NoticeType.BECOME_LEADER) {
+            System.out.println("I am " + getName() + ". I know " + student.getName() + " become Leader of Class " + klass.getNumber() + ".");
+        }
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klass) {
+        super(id, name, age);
+        this.klass = klass;
+        for (Klass klass1 : klass) {
+            klass1.attach(this);
+        }
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klass;
+    }
+
+    public void setKlass(LinkedList<Klass> klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce() + TEACHER + " I teach No Class.";
+        } else {
+            List<String> collect = klass.stream()
+                    .map(s -> s.getNumber().toString())
+                    .collect(Collectors.toList());
+            return super.introduce() + TEACHER + " I teach Class " + String.join(", ", collect) + ".";
+        }
+    }
+
+    public boolean isTeaching(Student student) {
+        return isIn(student);
+    }
+
+    public boolean isIn(Student student) {
+        for (Klass klass1 : klass) {
+            if (klass1.isIn(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String introduceWith(Student student) {
+        if (klass.stream()
+                .filter(s -> s.equals(student.getKlass()))
+                .collect(Collectors.toList()).size() != 0) {
+            return super.introduce() + TEACHER + " I teach " + student.getName() + ".";
+        }
+        return super.introduce()  + TEACHER + " I don't teach " + student.getName() + ".";
+    }
 }
