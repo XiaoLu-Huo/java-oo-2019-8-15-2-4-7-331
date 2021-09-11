80bd591d2e49cb19c0cd85e4dd37559ce5cd92e3
huoxiaolu <1821100432@qq.com>
2021-09-11 13:51:16 +0800

    [huoxiaolu] feat : 完成practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,28 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private static final String STUDENT = " I am a Student.";
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + STUDENT + " I am at " + klass.getDisplayName() + ".";
+    }
 }
