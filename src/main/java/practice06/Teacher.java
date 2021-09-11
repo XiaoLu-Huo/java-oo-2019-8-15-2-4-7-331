d89987376c5ccf38ecda3f37007816bce9f2555a
huoxiaolu <1821100432@qq.com>
2021-09-11 13:28:55 +0800

    [huoxiaolu] feat : 完成practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce();
+        if (klass == null) {
+            return introduce + " I am a Teacher. I teach No Class.";
+        }
+        return introduce + " I am a Teacher. I teach Class " + klass + ".";
+    }
 }
